b49c50c1d247e3f52d50c94a6bcfd5e11ecf3a67
poseidon-code <pritamhalder05062000@gmail.com>
2021-11-19 14:04:29 +0530

    add: bs-05

---
+++ binary-search/05-square-root.cpp
@@ -0,0 +1,26 @@
+// PROBLEM: Sind square root of a number
+// https://leetcode.com/problems/sqrtx/
+// https://www.interviewbit.com/problems/square-root-of-integer/
+
+#include<iostream>
+
+using namespace std;
+
+// SOLUTION
+int sqrt (int x) {
+    long r = x;
+    while (r*r > x)
+        r = (r + x/r)/2;
+    return r;
+}
+
+
+int main() {
+    // INPUT :
+    int x = 8;
+
+    // OUTPUT :
+    cout<<sqrt(x)<<endl;
+
+    return 0;
+}
